7a2a9ef890a4b8aae7d43dea112f1821952d4b84
EC2 Default User <ec2-user@ip-172-31-19-185.ap-northeast-1.compute.internal>
2020-09-16 22:03:16 +0000

    お仕事カテゴリのモデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_15_220151) do
+ActiveRecord::Schema.define(version: 2020_09_16_215810) do
 
   create_table "handicaps", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -117,6 +117,12 @@ ActiveRecord::Schema.define(version: 2020_09_15_220151) do
   end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "service_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
