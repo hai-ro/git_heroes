0eb24a8d842c26fc81b9565ae2f2fcc09644a0ba
EC2 Default User <ec2-user@ip-172-31-19-185.ap-northeast-1.compute.internal>
2020-09-19 12:53:39 +0000

    Topsコントローラにindexアクションを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :offices
   devise_for :helpers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'tops#index'
 end
